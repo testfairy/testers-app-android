67bc8176da8a61c7e6efc67855038191d724e2ba
gmegidish <gil@megidish.net>
2016-11-15 11:10:56 +0200

    Added SDK

--- TestFairyApp/src/main/java/com/testfairy/app/MainActivity.java
+++ TestFairyApp/src/main/java/com/testfairy/app/MainActivity.java
@@ -13,14 +13,9 @@ import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.Window;
-import android.view.WindowManager;
 import android.webkit.*;
 import android.webkit.CookieManager;
 import android.widget.ProgressBar;
-import android.widget.LinearLayout;
-import android.content.Context;
 import android.widget.Toast;
 import android.webkit.JavascriptInterface;
 import android.accounts.Account;
@@ -31,25 +26,14 @@ import java.util.Map;
 
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.params.BasicHttpParams;
 import org.apache.http.util.EntityUtils;
 
 import org.apache.http.*;
 import org.apache.http.client.*;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.protocol.*;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.impl.client.BasicCookieStore;
-import org.apache.http.impl.cookie.BasicClientCookie;
-import org.apache.http.params.BasicHttpParams;
 import org.apache.http.protocol.*;
 import org.json.JSONObject;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.HashMap;
+import com.testfairy.TestFairy;
 
 public class MainActivity extends Activity {
 
@@ -221,6 +205,8 @@ public class MainActivity extends Activity {
 
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		TestFairy.begin(this, "b5f48b5b410537bbb8781507af019e07307f2eac");
 
 		progressBar = (ProgressBar) findViewById(R.id.progressBar);
 		progressBar.setVisibility(View.GONE);
